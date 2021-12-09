9300614d49a3c25641bb02cc7a0df09d5d1f1f8b
GitHub Action <action@github.com>
2021-12-09 03:02:23 +0000

    Template cleanup

--- src/main/kotlin/com/github/jerrymhlee/ghsample/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/jerrymhlee/ghsample/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.jerrymhlee.ghsample.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jerrymhlee.ghsample.MyBundle
 
 class MyApplicationService {
 
